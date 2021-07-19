525f3ac0a8ccd86e2db3c4d22028cb7c8ac7cb5e
codymart1986 <codymart1986@hotmail.com>
2021-07-19 09:04:13 -0700

    set up AI class by importing from player class, chose to use random int for the cpu choice and modified printed responses for the choice made by cpu

---
+++ ai.py
@@ -0,0 +1,20 @@
+from player import Player
+import random
+
+class AI(Player):
+    def __init__(self):
+        super().__init__()
+
+    def cpu_choice(self):
+        cpu_choice = random.randint(1, 5)
+
+        if self.cpu_choice == 1:
+            print("CPU has chosen Rock")
+        elif self.cpu_choice == 2:
+            print("CPU has chosen Paper")
+        elif self.cpu_choice == 3:
+            print("CPU has chosen Scissors")
+        elif self.cpu_choice == 4:
+            print("CPU has chosen Lizard")
+        elif self.cpu_choice == 5:
+            print("CPU has chosen Spock")
