03830fb47e7599aa1adb2bf818b2c7dbe2127ef4
codymart1986 <codymart1986@hotmail.com>
2021-07-19 08:41:15 -0700

    continuing to set up player class. defined all the inputs for all gestures.

--- player.py
+++ player.py
@@ -5,4 +5,20 @@ class Player:
         self.score = 0
 
     def player_choice(self):
-        self.player_choice = input
+        self.player_choice = int(input("Select a gesture!"))
+        
+        if self.player_choice < 1 or self.player_choice > 5:
+            print("Invalid input, try again!")
+            self.player_choice
+        elif self.player_choice == 1:
+            print("Rock")
+        elif self.player_choice == 2:
+            print("Paper")
+        elif self.player_choice == 3:
+            print("Scissors")
+        elif self.player_choice == 4:
+            print("Lizard")
+        elif self.player_choice == 5:
+            print("Spock")
+
+
